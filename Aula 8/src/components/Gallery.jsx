76031fd63436bf6d3e0883deb6eda4840e888315
RaulBirlem <raul.birlem@gmail.com>
2024-09-26 21:46:46 -0300

    add-aula-8

---
+++ Aula 8/src/components/Gallery.jsx
@@ -0,0 +1,14 @@
+import Profile from "./Profile"
+const Gallery = () => {
+  return (
+    <>
+     <h1>Amazing scientists</h1>
+     <Profile/>
+     <Profile/>
+     <Profile/>
+
+
+    </>
+  )
+}
+export default Gallery
